1df80a2ce43060f62d60e2eb683427fb53c8e688
Raman Yadav <ramanydv795@gmail.com>
2026-08-14 15:48:51 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,10 +33,10 @@ function App() {
       const formData = new FormData();
       formData.append("file", file);
 
-      const response = await axios.post(
-        "http://127.0.0.1:8000/predict",
-        formData
-      );
+     const response = await axios.post(
+  `${import.meta.env.VITE_API_URL}/predict`,
+  formData
+);
 
       setResult(response.data);
     } catch (err) {
